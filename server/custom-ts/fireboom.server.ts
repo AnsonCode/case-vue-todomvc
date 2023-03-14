551e8fa5ded5d7677bac119f0607e4d0c8249ace
erguotou <erguotou525@gmail.com>
2023-03-14 16:18:02 +0800

    feat: Add github action

--- server/custom-ts/fireboom.server.ts
+++ server/custom-ts/fireboom.server.ts
@@ -3,27 +3,19 @@ import { configureWunderGraphServer, type AuthenticationResponse } from 'fireboo
 import type { HooksConfig } from './generated/fireboom.hooks';
 import type { InternalClient } from './generated/fireboom.internal.client';
 
-    
 
 export default configureWunderGraphServer<HooksConfig, InternalClient, {}>(() => ({
-   hooks:{  authentication: {
-				
-            }, queries:{},mutations:{}, },  graphqlServers: [{
-			apiNamespace: "gql",
-			serverName: "gql",
-			enableGraphQLEndpoint:true,
-			schema: new GraphQLSchema({
-				query: new GraphQLObjectType({
-					name: 'RootQueryType',
-					fields: {
-						hello: {
-							type: GraphQLString,
-							resolve() {
-								return 'world';
-							},
-						},
-					},
-				}),
-			}),
-		},]
+    hooks: {
+        global: {
+        },
+        authentication: {
+    
+    
+        },
+        queries: {
+        },
+        mutations: {
+        },
+    },
+    graphqlServers: []
 }));
