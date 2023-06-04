c31a34aad452d9feb6d8e458a704a466a33ac4f8
anson <caoyanbin1993@foxmail.com>
2023-06-04 22:16:23 +0800

    05

--- vue/src/apis.ts
+++ vue/src/apis.ts
@@ -33,19 +33,20 @@ export async function request<T>(url: string, opts?: RequestOptions) {
   }
 }
 
+// 「实时]获取全部待做事项
 export async function getAllTodos(callback: (data: TodoItem[]) => void) {
-  const resp = await fetch(`${baseUrl}/operations/GetAllTodos?wg_live=true`)
+  const resp = await fetch(`${baseUrl}/operations/GetAllTodos?wg_live=true`);
   if (resp.ok) {
-    const reader = resp.body?.pipeThrough(new TextDecoderStream()).getReader()
+    const reader = resp.body?.pipeThrough(new TextDecoderStream()).getReader();
     while (true) {
-      const read = await reader?.read()
+      const read = await reader?.read();
       try {
         if (read?.done) {
-          break
+          break;
         }
         if (read?.value) {
-          const json = JSON.parse(read.value)
-          callback(json.data.data as TodoItem[])
+          const json = JSON.parse(read.value);
+          callback(json.data.data as TodoItem[]);
         }
       } catch (error) {
         //
@@ -53,33 +54,47 @@ export async function getAllTodos(callback: (data: TodoItem[]) => void) {
     }
   }
 }
-
+// 完成单个待做事项
 export async function completedTodo(id: ID, completed: boolean) {
-  const { error } = await request('CompleteTodo', { id, completed, method: 'post' })
-  return !error
+  const { error } = await request("CompleteTodo", {
+    id,
+    completed,
+    method: "post",
+  });
+  return !error;
 }
-
+// 完成所有待做事项
 export async function completedAllTodos(completed: boolean) {
-  const { error } = await request('CompleteAllTodos', { completed, method: 'post' })
-  return !error
+  const { error } = await request("CompleteAllTodos", {
+    completed,
+    method: "post",
+  });
+  return !error;
 }
-
+// 创建待做事项
 export async function createTodo(title: string) {
-  const { error, data } = await request('CreateTodo', { method: 'post', title })
-  return error ? false : data
+  const { error, data } = await request("CreateTodo", {
+    method: "post",
+    title,
+  });
+  return error ? false : data;
 }
-
+// 更新待做事项
 export async function updateTitle(id: string, title: string) {
-  const { error } = await request('UpdateTodoTitle', { method: 'post', id, title })
-  return !error
+  const { error } = await request("UpdateTodoTitle", {
+    method: "post",
+    id,
+    title,
+  });
+  return !error;
 }
-
+// 删除待做事项
 export async function removeTodo(id: string) {
-  const { error } = await request('RemoveTodo', { method: 'post', id })
-  return !error
+  const { error } = await request("RemoveTodo", { method: "post", id });
+  return !error;
 }
-
+// 删除已完成的待做事项
 export async function removeCompleted() {
-  const { error } = await request('RemoveCompleted', { method: 'post' })
-  return !error
+  const { error } = await request("RemoveCompleted", { method: "post" });
+  return !error;
 }
